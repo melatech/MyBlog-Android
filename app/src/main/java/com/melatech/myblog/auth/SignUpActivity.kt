b247c07e93a1d91788e16a5a4ab1bfddb63ab008
melatech <maincontractor@gmail.com>
2020-08-31 19:28:18 +0100

    Login & SignUp Layout

---
+++ app/src/main/java/com/melatech/myblog/auth/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.melatech.myblog.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.melatech.myblog.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
